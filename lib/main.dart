4aed39c116258637a52795d077a8121e271690bf
21120569 <21120569@student.hcmus.edu.vn>
2024-04-24 23:35:51 +0700

    change homepage

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       child:  MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Story App',
-        home: MainPage(),
+        home: HomeScreen(),//MainPage(),
       ),
 
     );
