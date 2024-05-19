a0bc34df2900cda11adafcddfdc1f7633e860e49
21120569 <21120569@student.hcmus.edu.vn>
2024-05-19 22:55:37 +0700

    fix bug

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
 
-
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
